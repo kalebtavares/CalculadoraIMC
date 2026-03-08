ee8871e8e67e5edf6faf38872ddbe8f13a946229
kalebtavares <kalebtavares@ucloud.com>
2026-03-08 11:58:08 -0400

    Melhorias na saida dos dados de IMC.

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -15,26 +15,26 @@ public class Main {
         double altura = Double.parseDouble(JOptionPane.showInputDialog("Digite a sua altura"));
 
         double imc = peso / (altura * altura);
-        System.out.printf("Seu IMC calculado foi:  %.2f%n", imc);
+        String formatado = String.format("%.2f", imc);
 
         if (imc < 18.5) {
-            JOptionPane.showMessageDialog(null,"Você Está abaixo do Peso");
+            JOptionPane.showMessageDialog(null,"Seu IMC é: %.2f%n "+ formatado +" Você está abaixo do peso");
 
         } else if (imc >= 18.5 && imc <= 24.9) {
-            JOptionPane.showMessageDialog(null,"Seu peso está normal");
+            JOptionPane.showMessageDialog(null,"Seu IMC é: %.2f%n"+ formatado +" Seu peso está normal");
 
         } else if (imc >= 25.0 && imc <= 29.9) {
-            JOptionPane.showMessageDialog(null,"Você está com sobrepeso");
+            JOptionPane.showMessageDialog(null,"Seu IMC é: %.2f%n"+ formatado +" Você está com sobrepeso");
             
         } else if (imc >= 30.0 && imc <= 34.9) {
-            JOptionPane.showMessageDialog(null,"Você está com Obesidade grau I");
+            JOptionPane.showMessageDialog(null,"Seu IMC é:"+ formatado + " Você está com Obesidade grau I");
             
         } else if (imc >= 35.0 && imc <= 39.9) {
-            JOptionPane.showMessageDialog(null,"Você está com obesidade grau II");
+            JOptionPane.showMessageDialog(null,"Seu IMC é: "+ formatado + " Você está com obesidade grau II");
             
         }else {
 
-            JOptionPane.showMessageDialog(null,"Você está com Obesidade Grave!");
+            JOptionPane.showMessageDialog(null," Você está com Obesidade Grave!");
         }
 
 
