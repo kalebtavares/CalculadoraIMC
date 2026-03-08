c6d8af9c1aa501a73caa87df054022587914db19
kalebtavares <kalebtavares@ucloud.com>
2026-03-07 22:31:32 -0400

    Implementação da classe Main com todas os metodos para o funcionamento da calculdora de IMC

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -1,4 +1,45 @@
 package Program;
 
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
+    static void main() {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Vamos Calcular seu IMC");
+        System.out.println("Digite seu Peso");
+
+        double peso = scanner.nextDouble();
+        System.out.println("Digite a sua Altura");
+        double altura = scanner.nextDouble();
+
+        double imc = peso / (altura * altura);
+        System.out.printf("Seu IMC calculado foi:  %.2f%n", imc);
+
+        if (imc < 18.5) {
+            System.out.println("Você está abaixo do Peso");
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            System.out.println("Seu peso Está normal");
+
+        } else if (imc >= 25.0 && imc <= 29.9) {
+            System.out.println("Você está com sobrepeso");
+            
+        } else if (imc >= 30.0 && imc <= 34.9) {
+            System.out.println("Você está com obesidade grau I");
+            
+        } else if (imc >= 35.0 && imc <= 39.9) {
+            System.out.println("Você está com obesidade grau II");
+            
+        }else {
+
+            System.out.println("Você está com obesidade grave");
+        }
+
+        scanner.close();
+
+    }
 }
