3d5448a2fac0bea309455601607d2803b4576354
kalebtavares <kalebtavares@ucloud.com>
2026-03-08 07:54:13 -0400

    Implementado JOption Pane como método de saida, substituindo o método de saida pelo terminal.

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -22,22 +22,23 @@ public class Main {
         System.out.printf("Seu IMC calculado foi:  %.2f%n", imc);
 
         if (imc < 18.5) {
-            System.out.println("Você está abaixo do Peso");
+            JOptionPane.showMessageDialog(null,"Você Está abaixo do Peso");
+
         } else if (imc >= 18.5 && imc <= 24.9) {
-            System.out.println("Seu peso Está normal");
+            JOptionPane.showMessageDialog(null,"Seu peso está normal");
 
         } else if (imc >= 25.0 && imc <= 29.9) {
-            System.out.println("Você está com sobrepeso");
+            JOptionPane.showMessageDialog(null,"Você está com sobrepeso");
             
         } else if (imc >= 30.0 && imc <= 34.9) {
-            System.out.println("Você está com obesidade grau I");
+            JOptionPane.showMessageDialog(null,"Você está com Obesidade grau I");
             
         } else if (imc >= 35.0 && imc <= 39.9) {
-            System.out.println("Você está com obesidade grau II");
+            JOptionPane.showMessageDialog(null,"Você está com obesidade grau II");
             
         }else {
 
-            System.out.println("Você está com obesidade grave");
+            JOptionPane.showMessageDialog(null,"Você está com Obesidade Grave!");
         }
 
         scanner.close();
